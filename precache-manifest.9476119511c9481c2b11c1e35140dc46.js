a5efca5a977675287ed7c1ca2794a53d5a9d60e8
Martin Mumford <arcanemx@gmail.com>
2019-10-26 21:20:06 -0600

    Updates

---
+++ precache-manifest.9476119511c9481c2b11c1e35140dc46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44485ed0b29c2622315d93ae81957fa",
+    "url": "/welcome/index.html"
+  },
+  {
+    "revision": "11db33e24519fb9534cf",
+    "url": "/welcome/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "955ca3dc41d5c2211a67",
+    "url": "/welcome/static/js/2.d463e89a.chunk.js"
+  },
+  {
+    "revision": "11db33e24519fb9534cf",
+    "url": "/welcome/static/js/main.8d653ebe.chunk.js"
+  },
+  {
+    "revision": "5e514e168af6ec6960ef",
+    "url": "/welcome/static/js/runtime-main.ae3fe65f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/welcome/static/media/logo.25bf045c.svg"
+  }
+]);
